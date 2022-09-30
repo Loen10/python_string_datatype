4cbf9c422e893f18ee5f23ff0c011ca9db49c778
Logan Nance <ldnance10@gmail.com>
2022-09-29 18:37:36 -0600

    Tis done

--- python_strings.py
+++ python_strings.py
@@ -31,14 +31,14 @@ print(my_last_name[-4])
 print(my_first_name[:1])
 print(my_last_name[-2:])
 
-
 #TODO Combining Strings
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
 #       -first name six times
 
-print("{} {}".format(my_first_name,my_last_name))
-print(6 * my_first_name)
+print("{} {}".format(my_first_name, my_last_name))
+print((my_first_name + "\n") * 6)
+
 
 # TODO Formatting Strings
 #   - Print the following items (one per line) (print using variables)
@@ -46,7 +46,8 @@ print(6 * my_first_name)
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
 
 print("{} {} -was born in- {}".format(my_first_name, my_last_name, my_year_of_birth))
-print("{0} {1} -was born in- {2}. {0} -enjoyed celebrating- {3}".format(my_first_name, my_last_name, my_year_of_birth, current_year))
+print("{0} {1} -was born in- {2}. {0} -enjoyed celebrating- {3}"\
+    .format(my_first_name, my_last_name, my_year_of_birth, current_year))
 # TODO Escape characters
 #   - Print the following items (one per line) (print using variables)
 #       - possesive first name -birth year is- year of birth 
@@ -61,6 +62,6 @@ print("\t{} {}".format(my_last_name, current_year))
 #       - length of last name
 #       - first name and last name all in upper case
 
-print("{} {}".format(my_first_name, my_last_name).lower())
+print("{} {}".format(my_first_name, my_last_name).casefold())
 print(len(my_last_name))
 print("{} {}".format(my_first_name, my_last_name).upper())
